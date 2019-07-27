5a8c73e4942d86913adf692f585369408409c197
Eran Simhon <erans995@gmail.com>
2019-07-27 18:11:26 +0300

    Done coding the Thomas and Bob classes, should move on to chapter 14

--- Thomas was Late/Draw.cpp
+++ Thomas was Late/Draw.cpp
@@ -12,6 +12,10 @@ void Engine::draw()
 		m_Window.draw(m_BackgroundSprite);
 		// Switch to m_MainView
 		m_Window.setView(m_MainView);
+		// Draw thomas
+		m_Window.draw(m_Thomas.getSprite());
+		// Draw bob
+		m_Window.draw(m_Bob.getSprite());
 	}
 	else
 	{
@@ -23,6 +27,10 @@ void Engine::draw()
 		m_Window.draw(m_BackgroundSprite);
 		// Switch to m_LeftView
 		m_Window.setView(m_LeftView);
+		// Draw thomas
+		m_Window.draw(m_Thomas.getSprite());
+		// Draw bob
+		m_Window.draw(m_Bob.getSprite());
 		// Now draw Bob's side of the screen
 		// Switch to background view
 		m_Window.setView(m_BGRightView);
@@ -30,6 +38,10 @@ void Engine::draw()
 		m_Window.draw(m_BackgroundSprite);
 		// Switch to m_RightView
 		m_Window.setView(m_RightView);
+		// Draw thomas
+		m_Window.draw(m_Thomas.getSprite());
+		// Draw bob
+		m_Window.draw(m_Bob.getSprite());
 	}
 	// Draw the HUD
 	// Switch to m_HudView
